b4f7a90c63addd0858afdda50e59ca57908cbbfd
eakaran <31976448+eakaran@users.noreply.github.com>
2017-09-22 16:41:15 -0700

    Started plotting exercise

---
+++ PlottingExercise1.R
@@ -0,0 +1,11 @@
+# Elizabeth Karan
+# Plotting Exercise 1
+
+# 1
+get_heights <- function(heights){
+  heights <- rnorm(1000, mean = 69, sd = 10)
+  return(heights)
+}
+
+heights<- get_heights(heights)
+
