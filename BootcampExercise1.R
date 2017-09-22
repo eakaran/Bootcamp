aa50bc77645edfa6a929fddce3ffb80c1e67ef71
eakaran <31976448+eakaran@users.noreply.github.com>
2017-09-22 13:55:38 -0700

    Started exercise 1

---
+++ BootcampExercise1.R
@@ -0,0 +1,11 @@
+# Elizabeth Karan
+# Bootcamp Exercise 1
+
+# Exercise 1) Write a for loop statements so that it runs from 1:9 and prints the following output to your screen:
+for (j in 1:10){
+  cat("\n")
+  if(j==9){
+    cat("*")
+  }
+}
+
